0e92dd1bbacf51ca2776c11f62f5620f65b01b26
ameliaabernethy2003-lang <amelia.abernethy2003@gmail.com>
2026-02-11 20:43:58 -0500

    Update admin password to SG&A2026! and fix Supabase import for Netlify deployment

--- temp-next-app/src/app/admin/edit/page.tsx
+++ temp-next-app/src/app/admin/edit/page.tsx
@@ -24,7 +24,7 @@ type Submission = {
   processed?: boolean;
 };
 
-const ADMIN_PASSWORD = 'SG&A'; // Change this to your desired admin password
+const ADMIN_PASSWORD = 'SG&A2026!'; // Change this to your desired admin password
 const ADMIN_VENMO_USERNAME = 'peerpointtutors'; // Admin's Venmo username - make sure you're logged into Venmo with this account when paying tutors
 
 type Tutor = {
